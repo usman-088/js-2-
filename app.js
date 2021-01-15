7ffa978ff9d2be6e2548cafad2f2283d7ec8d256
Muhammad_Usman <usmanali14815@gmail.com>
2021-01-15 17:42:03 +0500

    javascript assignment 2

---
+++ app.js
@@ -0,0 +1,100 @@
+
+
+
+
+// var a = prompt("Enter your name")
+// alert(a.toUpperCase())
+// alert(a.toLowerCase())
+
+
+// var a = prompt("Enter your Phone Model")
+// alert(a.length)
+// var b = a.charAt(a.length-1)
+// alert(b)
+
+
+
+// var a = "Pakistani"
+// var b = a.indexOf("n")
+// alert(b)
+
+// var a = prompt("Enter user name")
+// if(a != "@" && a != "!" && a != "$" && a != "," && a != "."  ){
+//     prompt(" ","invalid username")
+// }
+
+
+
+// var a =  "The quit brown fox jumps over the lazy dog"
+// var b=1
+// for(var i=1;i<=a.length;i++){
+//     if(i=="the"||i=="The"){
+//         b+=i
+//     }
+
+// }
+// console.log(b)
+
+
+
+// var a = "Pakistani"
+// alert(a.charAt("3"))
+
+// var a = "Hyderabad"
+// alert(a.replace("Hyder","Islam"))
+
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var b = message.replace(/and/g, "&")
+// alert(b)
+
+
+
+// var n = 3.4512;
+// console.log("Number "+n+"\nRound off #"+Math.round(n)+"\nFloor #"+Math.floor(n)+"\nCeil #"+Math.ceil(n))
+
+// var n = -2.678;
+// console.log("Number "+n+"\nRound off #"+Math.round(n)+"\nFloor #"+Math.floor(n)+"\nCeil #"+Math.ceil(n))
+
+
+
+// var dice = 1
+// for(var i=1;i<=6;i++){
+//     dice+=Math.round(Math.random()) 
+// if(dice > 6){
+//     dice = 1
+//     dice+=Math.round(Math.random()+i) 
+// }
+// }
+// console.log(dice)
+
+
+// var n = Math.round(Math.random()+1) 
+// if(n==2){
+//     console.log("Head")
+// }
+// else{
+//     console.log("Tail")
+// }
+
+
+// var n = "50.2kg"
+// var a = parseFloat(n)
+// console.log(a)
+
+
+// var n = "427"
+// var a = parseInt(n)
+// console.log(a)
+// console.log(typeof a)
+
+
+// var n = 35.36 
+// var a = n.toString()
+// var replace = a.replace(".","")
+// console.log(replace)
+
+
+var per = 30/45*100
+console.log(per.toFixed(2))
+
